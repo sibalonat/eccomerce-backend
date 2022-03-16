e02407fb630040a50b2f09502beee455606a3e27
marininik <m.nikolli@lswr.it>
2022-03-16 12:58:50 +0100

    total revenue

--- routers/orders.js
+++ routers/orders.js
@@ -37,7 +37,16 @@ router.post(`/`, async (req, res) => {
         newOrderItem = await newOrderItem.save();
         return newOrderItem._id;
     }));
+    // req.body.totalPrice
     const orderItemsIdsResolved = await orderItemsIds;     
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        
+        return totalPrice;
+    }));
+
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
 
     let order = new Order({
         // orderItems: req.body.orderItems,
@@ -49,7 +58,7 @@ router.post(`/`, async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     }); 
     order = await order.save()
@@ -58,5 +67,47 @@ router.post(`/`, async (req, res) => {
     }
     res.send(order); 
 });
+
+router.put(`/:id`, async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        {
+            new: true
+        }
+    );
+    if (!order) 
+        return res.status(400).send('The order cannot be updated')
+    res.send(order);
+});
+
+router.delete(`/:id`, (req, res) => {
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if(order) {
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            });
+            return res.status(200).json({success: true, message: 'order deleted'})
+        } else {
+            return res.status(404).json({success: false, message: 'order is not deleted'})
+        }
+    }).catch(err => {
+        return res.status(500).json({success: false, error: err});
+    });
+});
+
+router.get('/get/totalsales', async (req, res) => {
+    const totalSales = await Order.aggregate([
+        {
+            $group: {_id: null, totalsales: {$sum:'$totalPrice'}}
+        }
+    ]);
+    if (!totalSales) {
+        return res.status(400).send('The sales orders cannot be generated')
+    }
+    res.send({totalSales: totalSales.pop().totalsales});
+})
 
 module.exports = router;
